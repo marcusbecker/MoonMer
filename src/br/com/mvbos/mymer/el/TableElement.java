1d3ec079afd85a442f78f8d2ef8d4eecb981aac8
Marcus Becker <marcusvbecker@gmail.com>
2016-09-18 20:37:39 -0300

    Moved to the branch

--- src/br/com/mvbos/mymer/el/TableElement.java
+++ src/br/com/mvbos/mymer/el/TableElement.java
@@ -7,9 +7,6 @@ package br.com.mvbos.mymer.el;
 
 import br.com.mvbos.jeg.element.ElementModel;
 import br.com.mvbos.mymer.Common;
-import br.com.mvbos.mymer.el.draw.FullDraw;
-import br.com.mvbos.mymer.el.draw.SimpleDraw;
-import br.com.mvbos.mymer.el.draw.TinyDraw;
 import br.com.mvbos.mymer.entity.EntityUtil;
 import br.com.mvbos.mymer.xml.field.Field;
 import br.com.mvbos.mymer.xml.field.Table;
@@ -37,16 +34,21 @@ public class TableElement extends ElementModel {
     public static final Font typeFont = new Font("Arial", Font.PLAIN, 12);
     public static final Font fitFont = new Font("Arial", Font.PLAIN, 10);
 
+    private static final int LEFT_BORDER = 5;
+
+    private short headerSize = 10;
+    private short fieldSize = 10;
+    //Field index type size
+    private short fitSize;
     private DataBaseElement dataBase;
     private String description;
     private List<Field> fields = new ArrayList<>(10);
 
     private boolean autoWidth = true;
     private boolean autoHeight = true;
+    private Color headerColor = Color.DARK_GRAY;
 
     private State state = State.NONE;
-
-    private final SimpleDraw[] drawModes;
 
     public TableElement(DataBaseElement dataBase, Table tb) {
         this(0, 0, 50, 50, dataBase, tb.getName(), tb.getDescription());
@@ -77,11 +79,6 @@ public class TableElement extends ElementModel {
 
             this.setColor(dataBase.getColor());
         }
-
-        drawModes = new SimpleDraw[3];
-        drawModes[0] = new SimpleDraw(this);
-        drawModes[1] = new FullDraw(this);
-        drawModes[2] = new TinyDraw(this);
     }
 
     public void addFields(Field field) {
@@ -114,14 +111,98 @@ public class TableElement extends ElementModel {
 
     @Override
     public void update() {
-        SimpleDraw sd = getSelectedDrawMode();
-        sd.update(Common.graphics);
+        if (Common.graphics != null) {
+
+            Graphics2D g = Common.graphics;
+
+            int c = (getColor().getRed() + getColor().getGreen() + getColor().getBlue()) / 3;
+            headerColor = c < 128 ? Color.LIGHT_GRAY : Color.DARK_GRAY;
+
+            if (autoHeight) {
+
+                headerSize = (short) g.getFontMetrics(headerFont).getHeight();
+                fieldSize = (short) (g.getFontMetrics(textFont).getHeight() + LEFT_BORDER);
+
+                if (isCrop()) {
+                    setHeight(fieldSize * (fields.size() > Common.maxRow ? Common.maxRow + 1 : fields.size()) + headerSize + 20);
+                } else {
+                    setHeight(fieldSize * fields.size() + headerSize + 20);
+                }
+            }
+
+            if (autoWidth) {
+                String maxWidth = getName();
+
+                Common.ct = 0;
+
+                for (Field f : fields) {
+                    if (f.getName().length() > maxWidth.length()) {
+                        maxWidth = f.getName();
+                    }
+
+                    if (isCrop()) {
+                        if (++Common.ct == Common.maxRow) {
+                            break;
+                        }
+                    }
+                }
+
+                fitSize = (short) g.getFontMetrics(fitFont).stringWidth("<PU> ");
+                int w = g.getFontMetrics(headerFont).stringWidth(maxWidth) + LEFT_BORDER + fitSize;
+
+                //w += g.getFontMetrics(typeFont).stringWidth(" char");
+                char data[] = new char[Common.typeCharLength + 1];
+                w += g.getFontMetrics(typeFont).charsWidth(data, 0, data.length);
+
+                setWidth(w);
+            }
+        }
     }
 
     @Override
     public void drawMe(Graphics2D g) {
-        SimpleDraw sd = getSelectedDrawMode();
-        sd.draw(g);
+
+        g.setColor(Color.DARK_GRAY);
+        g.drawRoundRect(getPx(), getPy(), getWidth(), getHeight(), 4, 4);
+
+        g.setColor(getColor());
+        g.fillRect(getPx(), getPy(), getWidth(), getHeight());
+
+        g.setFont(headerFont);
+        g.setColor(headerColor);
+        g.drawString(getName(), getPx() + LEFT_BORDER, getPy() + headerSize);
+
+        g.setColor(Color.WHITE);
+        g.fillRect(getPx() + 2, getPy() + headerSize + 10, getWidth() - 4, getHeight() - 35);
+
+        for (short i = 0; i < fields.size(); i++) {
+            Field f = fields.get(i);
+
+            char[] arr = Field.getIndexInitial(f);
+
+            if (arr != null) {
+                g.setFont(fitFont);
+                g.setColor(Color.LIGHT_GRAY);
+                g.drawString(String.format("<%c%c>", arr[0], arr[1]), getPx() + LEFT_BORDER, getPy() + fieldSize * (i + 2));
+            }
+
+            g.setFont(textFont);
+            g.setColor(Color.DARK_GRAY);
+
+            if (isCrop() && i == Common.maxRow) {
+                g.setFont(textFont.deriveFont(Font.ITALIC));
+                g.drawString(String.format("... %d more", fields.size() - Common.maxRow), getPx() + LEFT_BORDER, getPy() + fieldSize * (i + 2));
+                break;
+            }
+
+            String s = f.getName();
+            //String s = String.format("%s %s", fields.get(i).getName(), fields.get(i).getType());
+            g.drawString(s, getPx() + LEFT_BORDER + fitSize, getPy() + fieldSize * (i + 2));
+
+            g.setFont(typeFont);
+            g.setColor(Color.BLUE);
+            g.drawString(f.getShortType(), getAllWidth() - Common.typeCharLength * 10, getPy() + fieldSize * (i + 2));
+        }
     }
 
     public boolean isAutoWidth() {
@@ -169,12 +250,8 @@ public class TableElement extends ElementModel {
         this.state = state;
     }
 
-    public boolean isCrop() {
+    private boolean isCrop() {
         return state != State.ALLWAYS_VISIBLE && Common.crop;
-    }
-
-    private SimpleDraw getSelectedDrawMode() {
-        return drawModes[0];
     }
 
 }
